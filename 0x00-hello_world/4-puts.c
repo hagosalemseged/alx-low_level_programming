ea63ea958c3ebb1197f71626b22cffef1c057f47
hagosalemseged <hagosalemeri@gmail.com>
2023-08-10 08:09:37 -0700

    4-inputs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
- * main-prints "Programming is like building a multilingual
- *puzzle, followed by a new line
- * return 0 (success)
- * success the main function
+ * main-prints "Programming is like building a multilingual puzzle, followed by a new line
+ *
+ * main - Entry Point
+ *
+ * return Always 0 (success)
 */
 int main(void)
 {
